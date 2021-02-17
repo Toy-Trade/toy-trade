3b03b3185b88a4e1923601cd5a50a1eee90369ad
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2021-02-17 13:31:28 -0500

    Render html using node

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('index.html', null, function(error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("Page not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 }).listen(3000, '127.0.0.1');
+
 console.log('Server running at 127.0.0.1:3000');
