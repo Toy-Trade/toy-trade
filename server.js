26c9cdb639de63069fbbed6d07a3bab75ac15f22
Joyce Fang <joycefang01@gmail.com>
2021-02-20 10:22:03 -0500

    Add public folder and set up express in server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('index.html', null, function(error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write("Page not found");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-}).listen(3000, '127.0.0.1');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log('Server running at 127.0.0.1:3000');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.send();
+});
+
+app.listen(port, () => {
+  console.log('Listening on *:3000');
+});
