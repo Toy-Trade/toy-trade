153524ff7d53ca8ad3f40d4e26576b8fdde35e81
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2021-03-10 13:12:13 -0500

    Handle add toy submit

--- public/script.js
+++ public/script.js
@@ -15,5 +15,15 @@ function signOut() {
 }
 
 function addToy() {
-  
+  var title = document.getElementById("title").value;
+  var estimatedValue = document.getElementById("estimated-value").value;
+  var brand = document.getElementById("brand").value;
+  var ageRange = document.getElementById("age-range").value;
+  var description = document.getElementById("description").value;
+
+  console.log("Title: " + title);
+  console.log("Estimated value: " + estimatedValue);
+  console.log("Brand: " + brand);
+  console.log("Age range: " + ageRange);
+  console.log("Description: " + description);
 }
