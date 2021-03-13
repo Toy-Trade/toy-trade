408aeb1648c7cfc6f5fa0685b6303e4301c49145
Joyce Fang <joycefang01@gmail.com>
2021-03-13 11:25:14 -0500

    Setting Up Angular
    Co-Authored-By: jssunray34 <55996087+jssunray34@users.noreply.github.com>
    Co-Authored-By: collcorr72 <46731655+collcorr72@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+app.use(express.static(path.join(__dirname, './toytrade/dist/toytrade')));
 
 app.use(express.static("public"));
 
