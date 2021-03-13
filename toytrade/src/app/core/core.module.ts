0131b04d7dbbca6fed3be319d5d947c0b71e92c3
Joyce Fang <joycefang01@gmail.com>
2021-03-13 12:31:43 -0500

    Create components in core module
    Co-Authored-By: jssunray34 <55996087+jssunray34@users.noreply.github.com>
    Co-Authored-By: collcorr72 <46731655+collcorr72@users.noreply.github.com>

--- toytrade/src/app/core/core.module.ts
+++ toytrade/src/app/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderLandingComponent } from './universal/header-landing/header-landing.component';
+import { HeaderComponent } from './universal/header/header.component';
+import { FooterComponent } from './universal/footer/footer.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ChatPageComponent } from './pages/chat-page/chat-page.component';
+import { NotificationsPageComponent } from './pages/notifications-page/notifications-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderLandingComponent, HeaderComponent, FooterComponent, LandingPageComponent, AboutPageComponent, HomePageComponent, ChatPageComponent, NotificationsPageComponent, ProfilePageComponent],
   imports: [
     CommonModule
   ]
---
+++ toytrade/src/app/core/pages/notifications-page/notifications-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-page',
+  templateUrl: './notifications-page.component.html',
+  styleUrls: ['./notifications-page.component.css']
+})
+export class NotificationsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
