8e0527f1ec8323d9065c34691a21ce01ee5b1f4f
Joyce Fang <joycefang01@gmail.com>
2021-03-17 12:46:13 -0400

    Fix routing and default to landing page
    Co-Authored-By: jssunray34 <55996087+jssunray34@users.noreply.github.com>
    Co-Authored-By: collcorr72 <46731655+collcorr72@users.noreply.github.com>
    Co-Authored-By: TylerG00 <37565033+TylerG00@users.noreply.github.com>

--- toytrade/src/app/app-routing.module.ts
+++ toytrade/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AboutPageComponent } from './core/pages/about-page/about-page.component
 import { AddToyPageComponent } from './core/pages/add-toy-page/add-toy-page.component';
 import { ChatPageComponent } from './core/pages/chat-page/chat-page.component';
 import { HomePageComponent } from './core/pages/home-page/home-page.component';
+import { LandingPageComponent } from './core/pages/landing-page/landing-page.component';
 import { NotificationsPageComponent } from './core/pages/notifications-page/notifications-page.component';
 import { ProfilePageComponent } from './core/pages/profile-page/profile-page.component';
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent},
   { path: 'about', component: AboutPageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'chat', component: ChatPageComponent },
