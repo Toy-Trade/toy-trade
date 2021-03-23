9c6e5d1995b99d3c431b915442d115f15e3cb80b
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2021-03-23 09:37:02 -0400

    Google sign in working
    Co-Authored-By: joycexfang <55996042+joycexfang@users.noreply.github.com>

--- toytrade/src/app/app.component.ts
+++ toytrade/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toytrade';
+
+  constructor(public auth: AngularFireAuth) {
+  }
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
--- toytrade/src/app/app.module.ts
+++ toytrade/src/app/app.module.ts
@@ -7,6 +7,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfp86Dt9-L2hi1CqosDVCni8KxjDZgg0g",
+  authDomain: "toy-trade.firebaseapp.com",
+  projectId: "toy-trade",
+  storageBucket: "toy-trade.appspot.com",
+  messagingSenderId: "987017105694",
+  appId: "1:987017105694:web:1bf4950a9420d9150af90f",
+  measurementId: "G-QXM0FDMYQ4"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +30,10 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
