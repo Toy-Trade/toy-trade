3c2f9e9cf3d9e5c72b198fa6ae22f9172d92f3da
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2021-03-23 11:30:57 -0400

    Login and logout work for Google signin
    TODO: check for successful login before redirecting
    Co-Authored-By: joycexfang <55996042+joycexfang@users.noreply.github.com>
    Co-Authored-By: collcorr72 <46731655+collcorr72@users.noreply.github.com>
    Co-Authored-By: TylerG00 <37565033+TylerG00@users.noreply.github.com>

--- toytrade/src/app/shared/login-button/login-button.component.ts
+++ toytrade/src/app/shared/login-button/login-button.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-button',
@@ -8,14 +9,21 @@ import firebase from 'firebase/app';
   styleUrls: ['./login-button.component.css']
 })
 export class LoginButtonComponent implements OnInit {
+  @Input() blueButton: boolean = false;
+  buttonText: String = "Login";
 
-  constructor(public auth: AngularFireAuth) {
+  constructor(public auth: AngularFireAuth, private router: Router) {
   }
   login() {
+    this.buttonText = "Logout";
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    //TODO: Check for successful login
+    this.router.navigateByUrl("/home");
   }
   logout() {
+    this.buttonText = "Login";
     this.auth.signOut();
+    this.router.navigateByUrl("/");
   }
 
   ngOnInit(): void {
