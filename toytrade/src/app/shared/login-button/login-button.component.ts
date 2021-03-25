5da124cf9c8887ec618d9edaeadcdfb13746db84
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2021-03-24 20:51:09 -0400

    Check for successful login before navigating to home

--- toytrade/src/app/shared/login-button/login-button.component.ts
+++ toytrade/src/app/shared/login-button/login-button.component.ts
@@ -15,10 +15,21 @@ export class LoginButtonComponent implements OnInit {
   constructor(public auth: AngularFireAuth, private router: Router) {
   }
   login() {
-    this.buttonText = "Logout";
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    //TODO: Check for successful login
-    this.router.navigateByUrl("/home");
+    // this.buttonText = "Logout";
+    // this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    // //TODO: Check for successful login
+    // this.router.navigateByUrl("/home");
+    return this.AuthLogin(new firebase.auth.GoogleAuthProvider());
+  }
+  AuthLogin(provider) {
+    return this.auth.signInWithPopup(provider)
+    .then((result) => {
+        console.log('You have been successfully logged in!')
+        this.router.navigateByUrl("/home");
+        this.buttonText = "Logout";
+    }).catch((error) => {
+        console.log(error)
+    })
   }
   logout() {
     this.buttonText = "Login";
