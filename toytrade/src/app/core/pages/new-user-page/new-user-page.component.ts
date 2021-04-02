ec95790b00b8ccfe96ac2dd1fb730fcb87d92d58
Joyce Fang <joycefang01@gmail.com>
2021-04-01 22:53:11 -0400

    Created Add New User Form and added styling
    Co-Authored-By: jssunray34 <55996087+jssunray34@users.noreply.github.com>

--- toytrade/src/app/core/pages/new-user-page/new-user-page.component.ts
+++ toytrade/src/app/core/pages/new-user-page/new-user-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-new-user-page',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-user-page.component.css']
 })
 export class NewUserPageComponent implements OnInit {
+  addNewUserForm: FormGroup;
 
-  constructor() { }
+  constructor(public uauth: AuthService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  initializeForm(): void {
+    this.addNewUserForm = this.fb.group({
+      userId: [this.uauth.user.uid],
+      username: ['', [Validators.required]],
+      zipcode: ['', [Validators.required]],
+      bio: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    alert("clicked submit!");
+    let userToAdd = this.addNewUserForm.getRawValue();
+    console.log(userToAdd);
+  }
 }
