e7cba284a2c0cf2f83011146d9ec283e1d45af34
Joyce Fang <joycefang01@gmail.com>
2021-04-11 17:57:16 -0400

    Create and style new form for refining search
    Co-Authored-By: jssunray34 <55996087+jssunray34@users.noreply.github.com>

--- toytrade/src/app/core/pages/home-page/home-page.component.ts
+++ toytrade/src/app/core/pages/home-page/home-page.component.ts
@@ -9,6 +9,10 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HomePageComponent implements OnInit {
   user: string = this.uauth.user.displayName;
+  conditions: string[] = [ "New", "Used: Like New", "Used: Very Good", "Used: Good" ];
+  categories: string[] = [ "Action Figures", "Dolls", "Animals", "Cars", "Education", "Electronic Toys", "Games and Puzzles", "Other" ];
+  ageRanges: string[] = [ "1-2", "2-3", "4-5", "6-7", "8+" ];
+  brands: string[] = [ "Lego", "Barbie", "Hot Wheels", "American Girl", "Fisher-Price", "Nerf", "Hasbro", "Other" ];
 
   constructor(public uauth: AuthService) { }
 
