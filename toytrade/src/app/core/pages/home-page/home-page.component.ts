db05ec7b8c667f6d79fee1499e1f31068b8482e9
collcorr72 <46731655+collcorr72@users.noreply.github.com>
2021-04-21 12:04:54 -0400

    Added form basics

--- toytrade/src/app/core/pages/home-page/home-page.component.ts
+++ toytrade/src/app/core/pages/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn, ControlContainer } from '@angular/forms';
+import { timeStamp } from 'node:console';
+import { ExpectedConditions } from 'protractor';
 
 import { AuthService } from '../../services/auth.service';
 
@@ -9,12 +12,42 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HomePageComponent implements OnInit {
   user: string = this.uauth.user.displayName;
-  conditions: string[] = [ "New", "Used: Like New", "Used: Very Good", "Used: Good" ];
-  categories: string[] = [ "Action Figures", "Dolls", "Animals", "Cars", "Education", "Electronic Toys", "Games and Puzzles", "Other" ];
-  ageRanges: string[] = [ "1-2", "2-3", "4-5", "6-7", "8+" ];
-  brands: string[] = [ "Lego", "Barbie", "Hot Wheels", "American Girl", "Fisher-Price", "Nerf", "Hasbro", "Other" ];
+  // conditions: string[] = [ "New", "Used: Like New", "Used: Very Good", "Used: Good" ];
+  // categories: string[] = [ "Action Figures", "Dolls", "Animals", "Cars", "Education", "Electronic Toys", "Games and Puzzles", "Other" ];
+  // ageRanges: string[] = [ "1-2", "2-3", "4-5", "6-7", "8+" ];
+  // brands: string[] = [ "Lego", "Barbie", "Hot Wheels", "American Girl", "Fisher-Price", "Nerf", "Hasbro", "Other" ];
 
-  constructor(public uauth: AuthService) { }
+  form: FormGroup;
+  filterData = [
+    {title: 'conditions', value: 'New'},
+    {title: 'conditions', value: 'Used: Like New'},
+    {title: 'conditions', value: 'Used: Very Good'},
+    {title: 'conditions', value: 'Used: Good'},
+    {title: 'categories', value: 'Action Figures'}
+  ];
+
+  get filtersFormArray() {
+    return this.form.controls.orders as FormArray;
+  }
+
+  constructor(public uauth: AuthService, private formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      filters: new FormArray([])
+    });
+
+    this.addCheckBoxes();
+  }
+
+  private addCheckBoxes() {
+    this.filterData.forEach(() => this.filtersFormArray.push(new FormControl(false)));
+  }
+
+  submit() {
+    const selectedFilters = this.form.value.filters
+      .map((checked, i) => checked ? this.filterData[i].value : null)
+      .filter(v => v !== null);
+    console.log(selectedFilters);
+  }
 
   ngOnInit(): void {
   }
